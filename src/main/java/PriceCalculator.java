2b99f5610c8f2f7df4ef3e3fb4d4eff0ff9147b5
Wojciech Turek <turekwojtek@gmail.com>
2023-06-02 21:22:48 +0200

    develop test commmit

--- src/main/java/PriceCalculator.java
+++ src/main/java/PriceCalculator.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class PriceCalculator {
 
@@ -13,21 +14,27 @@ public class PriceCalculator {
 
     public BigDecimal calculatePrice(LocalDate dateFrom, LocalDate dateTo, Map<DayOfWeek, BigDecimal> carPriceList) {
 
-//        long numOfDaysBetween = ChronoUnit.DAYS.between(dateFrom, dateTo);
-//        List<DayOfWeek> daysListToCount = IntStream.iterate(0, i -> i + 1)
-//                .limit(numOfDaysBetween)
-//                .mapToObj(dateFrom::plusDays)
-//                .map(LocalDate::getDayOfWeek)
-//                .collect(Collectors.toList());
+        long numOfDaysBetween = ChronoUnit.DAYS.between(dateFrom, dateTo);
 
-        final List<DayOfWeek> daysListToCount = dateFrom.datesUntil(dateTo)
+        List<DayOfWeek> daysListToCount2 = IntStream.iterate(0, i -> i + 1)
+                .limit(numOfDaysBetween)
+                .mapToObj(dateFrom::plusDays)
                 .map(LocalDate::getDayOfWeek)
                 .collect(Collectors.toList());
-        System.out.println(daysListToCount);
 
+        List<DayOfWeek> daysListToCount3 = IntStream.iterate(0, i -> i + 1)
+            .limit(numOfDaysBetween)
+            .mapToObj(i -> dateFrom.plusDays(i))
+            .map(date -> date.getDayOfWeek())
+            .collect(Collectors.toList());
+
+        List<DayOfWeek> daysListToCount = dateFrom.datesUntil(dateTo)
+                .map(date -> date.getDayOfWeek())
+                .collect(Collectors.toList());
+        System.out.println(daysListToCount);
         return daysListToCount.stream()
-                .map(carPriceList::get)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
+                .map(dayOfWeek -> carPriceList.get(dayOfWeek))
+                .reduce(BigDecimal.ZERO, (subtotal, price) -> subtotal.add(price));
 
     }
 
